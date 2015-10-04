d0454051062fb2ac4fe3395abd04667b836d70db
Michelle Todd <michelle@khanacademy.org>
2015-10-04 10:36:59 -0700

    Style the buttons

--- components/Counter.ios.js
+++ components/Counter.ios.js
@@ -34,26 +34,25 @@ Button.PropTypes = {
   onPress: PropTypes.func.isRequired,
 };
 
-class Count extends Component {
+class AnimalCount extends Component {
   render() {
     var { count, names, increment, decrement } = this.props;
     return (
-      <View>
+      <View style={styles.animalCount}>
+        <Button onPress={increment}>+</Button>
         <Text style={styles.count}>
-          {count} {count == 1 ? names.singular : names.plural}
+          {count}
         </Text>
-        <Button onPress={increment}>
-          +
-        </Button>
-        <Button onPress={decrement}>
-          -
-        </Button>
+        <Text style={styles.animalName}>
+          {count == 1 ? names.singular : names.plural}
+        </Text>
+        <Button onPress={decrement}>&ndash;</Button>
       </View>
     );
   }
 }
 
-Count.PropTypes = {
+AnimalCount.PropTypes = {
   count: PropTypes.number.isRequired,
   type: PropTypes.string.isRequired,
   increment: PropTypes.func.isRequired,
@@ -70,7 +69,7 @@ class Counter extends Component {
     for (let animal in animals) {
       const animalData = animals[animal];
       animalCounts.push(
-        <Count
+        <AnimalCount
             key={animal}
             names={animals[animal].name}
             count={animals[animal].count}
@@ -98,27 +97,46 @@ Counter.PropTypes = {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
     backgroundColor: '#F5FCFF',
   },
+  animalCount: {
+    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+    borderTopWidth: 1,
+    flex: 1,
+    alignItems: 'stretch',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
   count: {
-    fontSize: 20,
+    fontSize: 32,
     textAlign: 'center',
-    margin: 10,
+  },
+  animalName: {
+    color: 'rgba(0, 0, 0, 0.5)',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
   },
   button: {
+    alignSelf: 'stretch',
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: 'rgba(0, 0, 0, 0.2)',
+    borderRadius: 4,
+    flex: 1,
     margin: 5,
-    padding: 5,
-    paddingLeft: 10,
-    paddingRight: 10,
+    height: 40,
   },
   buttonText: {
-    textAlign: 'center',
-    color: '#333333',
+    color: 'rgba(0, 0, 0, 0.5)',
+    fontSize: 26,
+    fontWeight: 'bold',
     marginBottom: 5,
+    textAlign: 'center',
   },
 });
 
